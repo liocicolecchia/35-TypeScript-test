887b2903c4abb9aefe8faa0e2039e95ea1766434
Lio <liocicolecchia@gmail.com>
2026-07-18 23:56:02 +0200

    more complex types

--- basics.ts
+++ basics.ts
@@ -12,4 +12,34 @@ let isInstructor: boolean;
 isInstructor = true;
 
 // More complex types: arrays, objects
+
+let hobbies: string[];
+
+hobbies = ["Sports", "Cooking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Lio",
+  age: 23,
+};
+
+let people: {
+  name: string;
+  age: number;
+}[];
+
+// person = {
+//   isEmployee: true,
+// };
+
+// Type Inference
+
+let course: string | number = "Im doing the course";
+
+course = 234534;
+
 // Functions types, parameters
