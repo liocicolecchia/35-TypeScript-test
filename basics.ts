58bf90c315802d36fdbd12127cab242e89d375c4
Lio <liocicolecchia@gmail.com>
2026-07-18 23:45:14 +0200

    the base types

---
+++ basics.ts
@@ -0,0 +1,15 @@
+// Primitives: numbers, strings, boolean
+let age: number;
+
+age = 12;
+
+let userName: string;
+
+userName = "Max";
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+// More complex types: arrays, objects
+// Functions types, parameters
